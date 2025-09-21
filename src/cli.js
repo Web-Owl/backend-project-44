bafc4bb4c74d7bad95c161b49d19216bf45488a3
Vladimir <v.savin@autodrive-agency.ru>
2025-09-21 04:34:53 +0300

    added eslint

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 export default () => {
-  readlineSync.question('May I have your name? ');
+  readlineSync.question('May I have your name? ')
 }
